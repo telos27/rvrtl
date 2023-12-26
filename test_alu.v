706a2c0eebcfe1c6b2f303c0356fe2758c429d99
telos27 <149587669+telos27@users.noreply.github.com>
2023-12-26 20:23:02 +0800

    Added test for Shifter32

--- test_alu.v
+++ test_alu.v
@@ -8,7 +8,7 @@ module test_alu() ;
 
 Adder_32 adder32(.a(a), .b(b), .sub(zero), .s(s), .overflow(overflow));
 
-Decoder_5to8 decoder(.s(s5), .shift(s32));
+Decoder_5to8 decoder(.s(b[4:0]), .shift(s32));
 
 Shifter_32 shifter32(.shift(s32) , .din(a) , .dleftout(shiftleft), .drightout(shiftright));
 
@@ -26,6 +26,7 @@ integer i; // Loop counter
 integer expected_sum ;
 integer expected_overflow ;
 integer expected_s32 ;
+integer expected_shiftleft , expected_shiftright ;
 integer seed = 100 ;
 
 assign zero = 0 ;
@@ -35,7 +36,7 @@ initial begin
     $dumpvars (0 , test_alu) ;
 
     //seed = 100 ;
-
+/*
     for (i=0 ; i<32 ; i= i+1) begin
       s5 = i ;
 
@@ -46,7 +47,7 @@ initial begin
         $display("ERROR: s5=0x%x , s32=0x%x , expected=0x%x" , s5 , s32 , expected_s32);
       end
     end
-
+*/
     // Randomly test for 1000 iterations
     for (i = 0; i < 10; i = i + 1) begin
       // Generate random 32-bit values for a and b
@@ -59,11 +60,20 @@ initial begin
       expected_sum = a + b;
       expected_overflow = (a+b<a);
 
+      expected_shiftright = (a)>>b[4:0] ;
+      expected_shiftleft = (a)<<b[4:0] ;
+
       #10;
 
       // Check if adder output matches expected values
       if (s!==expected_sum) $display("ERROR: Sum mismatch for %dth test case, s=%x , expected=%x", i , s , expected_sum);
       if (overflow!==expected_overflow) $display("ERROR: Overflow mismatch for %dth test case", i);
+
+      if (shiftright!==expected_shiftright) 
+        $display("ERROR: shift right mismatch for %dth test case, %x>>%d=%x , expected=%x", i , a , b[4:0] , shiftright , expected_shiftright);
+      if (shiftleft!==expected_shiftleft) 
+        $display("ERROR: shift left mismatch for %dth test case, %x<<%d=%x , expected=%x", i , a , b[4:0] , shiftleft , expected_shiftleft);
+
 
       // Print test case results
       $display("Test case %d passed: a=%x, b=%x, s=%x, overflow=%d", i, a, b, s, overflow);
