559587a2fcb3daa06324c519119726e0f119b650
HSDCLZ <624688597@qq.com>
2024-08-13 19:50:07 +0800

    debug Multiplier

--- ALU.v
+++ ALU.v
@@ -10,20 +10,35 @@ module ALU (clr, rs1, rs2, func7, func3, result, BranchALU);
     input [2:0] func3;
     output reg [31:0] result;
     output reg [2:0] BranchALU;
-    wire [31:0] rs2bar, muxrs2, sum, muxshift, shift, product;
+
+    wire [31:0] rs1_as, rs2_as, rs1_mu, rs2_mu, rs1_di, rs2_di, rs1_sh;
+    wire [4:0]  rs2_sh;
+    assign rs1_as = (~func7[0]&((func3==0)|(func3==2)|(func3==3))) ? rs1 : 32'h0;
+    assign rs2_as = (~func7[0]&((func3==0)|(func3==2)|(func3==3))) ? rs2 : 32'h0;
+    assign rs1_mu = ( func7[0]&~func3[2]) ? rs1 : 32'h0;
+    assign rs2_mu = ( func7[0]&~func3[2]) ? rs2 : 32'h0;
+    assign rs1_di = ( func7[0]& func3[2]) ? rs1 : 32'h0;
+    assign rs2_di = ( func7[0]& func3[2]) ? rs2 : 32'hffff_ffff;
+    assign rs1_sh = (~func7[0]&((func3==1)|(func3==5))) ? rs1 : 32'h0;
+    assign rs2_sh = (~func7[0]&((func3==1)|(func3==5))) ? rs2[4:0] : 4'h0;
+
+    //加减法
+    wire [31:0] rs2_as_in;
+    wire overflow;
+    assign rs2_as_in = func7[5] ? ~rs2_as : rs2_as;
+    Adder_32 Adder (.a(rs1_as), .b(rs2_as_in), .sub((func7[5]|func3[1])), .sum(sum), .overflow(overflow), .zeroflag(zero));
+
+    wire [63:0] product;
+    //乘法
+    Multiplier Multiplier (.a(rs1_mu), .b(rs2_mu), .sign(func3[1]), .prod(product), .overflow(overflow));
+
+    wire [31:0] quotient, remainder;
+    //除法
+    Division Division (.a(rs1_di), .b(rs2_di), .sign(func3[0]), .quotient(quotient), .remainder(remainder));
+
     wire [31:0] shiftresult;
-    wire overflow, zero, slt;
-    //rs2取反
-    assign rs2bar = ~rs2;
-    Mux_2_32 Mux_b (.select(func7[5]), .datain0(rs2), .datain1(rs2bar), .dataout(muxrs2));
-    //加法器
-    Adder_32 Adder (.a(rs1), .b(muxrs2), .sub(func7[5]), .sum(sum), .overflow(overflow), .zeroflag(zero));
-    //乘法器
-    //Multiplier Multiplier (.a(rs1), .b(rs2), .sign(func3[1]), .prod(product), .overflow());
-    //除法器
-    //Division Division (.a(rs1), .b(rs2), .sign(), .q(), .r());
-    //移位模块
-    Shifter_32 shifter32 (.shift5(rs2[4:0]), .datain(rs1), .right(func3==5), .sra(func7[5]), .dataout(shiftresult));
+    //移位
+    Shifter_32 shifter32 (.shift5(rs2_sh), .datain(rs1_sh), .right(func3==5), .sra(func7[5]), .dataout(shiftresult));
 
     assign slt = sum[31]^overflow;
     //ALU输出
@@ -31,7 +46,21 @@ module ALU (clr, rs1, rs2, func7, func3, result, BranchALU);
         if (clr) begin
             result <= 32'b0;
         end
-        case (func3)
+        else if (func7[0]) begin
+            case (func3)
+            0: result <= sum;                   //有符号乘法低位结果
+            1: result <= shiftresult;           //有符号乘法高位结果
+            2: result <= slt?32'b1:32'b0;       //
+            3: result <= overflow?32'b1:32'b0;  //无符号小于置1
+            4: result <= rs1 ^ rs2;             //逻辑异或
+            5: result <= shiftresult;           //右移
+            6: result <= rs1 | rs2;             //逻辑或
+            7: result <= rs1 & rs2;             //逻辑与
+            default: result <= 32'b0;
+            endcase
+        end
+        else begin
+            case (func3)
             0: result <= sum;                   //算数结果
             1: result <= shiftresult;           //左移
             2: result <= slt?32'b1:32'b0;       //小于置1
@@ -41,7 +70,8 @@ module ALU (clr, rs1, rs2, func7, func3, result, BranchALU);
             6: result <= rs1 | rs2;             //逻辑或
             7: result <= rs1 & rs2;             //逻辑与
             default: result <= 32'b0;
-        endcase
+            endcase
+        end
         //设置标志位
         BranchALU <=  ((func3==0) & zero)       //等于0
                     | ((func3==1) & ~zero)      //不等于0
--- test_Mul.v
+++ test_Mul.v
@@ -3,40 +3,38 @@
 `include "Adder_32.v"
 module test_Mul ();
 
-    Multiplier Mul (.a(a), .b(b), .sign(sign), .prod({prodh,prodl}), .overflow(overflow));
+    Multiplier Mul (.a(a), .b(b), .sign(sign), .prod(prod), .overflow(overflow));
 
     reg  [31:0] a;
     reg  [31:0] b;
     reg         sign;
-    wire [31:0] prodh;
-    wire [31:0] prodl;
+    wire [63:0] prod;
     wire        overflow;
 
-    integer i, j;
-    integer expected_product;
+    integer i, j, seed;
+    real expected_product;
 
     initial begin
         $dumpfile ("test_Mul.vcd");
         $dumpvars (0 , test_Mul);
 
-        sign = 1;
+        sign = 0;
 
-        for (i=0; i<64; i=i+1) begin
-            for (j=0; j<64; j=j+1) begin
-                a = i;
-                b = j;
-                expected_product = i * j;
+        i = -1;
+        j = -1;
+        a = i;
+        b = j;
+        expected_product = i * j;
 
-                #5
+        #5
 
-                if (prodl !== expected_product) begin
-                    $display("错误 %h * %h = %h , expected = %h", i , j , prodl , expected_product);
-                end
-                else begin
-                    $display("%h * %h = %h , expected = %h", i , j , prodl , expected_product);
-                end
-            end
+        if (prod == expected_product) begin
+            $display("%h * %h = %h , expected = %h", i , j , prod , expected_product);
         end
+        else begin
+            $display(" %h \n %h", prod , expected_product);
+        end
+
         $finish;
     end
 endmodule
